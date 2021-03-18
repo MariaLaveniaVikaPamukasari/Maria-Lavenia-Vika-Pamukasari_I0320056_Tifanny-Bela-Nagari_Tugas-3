77f4890e06b9fdc63aab80ea23813745debeb116
MariaLaveniaVikaPamukasari <vikalavenia@gmail.com>
2021-03-18 22:55:55 +0700

    membuat exercise 3.6

---
+++ I0320056_exercise3.6.py
@@ -0,0 +1,6 @@
+dict = {'Name':'Vicka','Age':18,'Class':'Second'}
+dict['Age'] = 19
+dict['School'] = "Universitas Sebelas Maret"
+
+print("dict['Age']:", dict['Age'])
+print("dict['School]:", dict['School'])
