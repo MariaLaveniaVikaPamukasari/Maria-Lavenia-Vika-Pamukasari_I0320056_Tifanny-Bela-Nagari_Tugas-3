9d4d8e61bb4fe55cc67f8a9c7f1c1a8f62bdfca6
MariaLaveniaVikaPamukasari <vikalavenia@gmail.com>
2021-03-19 21:22:03 +0700

    membuat exercise soal 1

---
+++ I0320056_soal1.py
@@ -0,0 +1,36 @@
+list = ['Issa','Hafiz','Naufal','Harry','Ivan','Elsa','Rian','Ilham','Diva','Asyer']
+print(list)
+print("list[4]:", list[4])
+print("list[6:8]:", list[6:8])
+print("Nilai ada pada index 3:", list[3])
+print("Nilai ada pada index 5:", list[5])
+print("Nilai ada pada index 9:", list[9])
+list[3] = ('Ojat')
+list[5] = ('Alam')
+list[9] = ('Maurich')
+print("Nilai baru ada pada index 3:", list[3])
+print("Nilai baru ada pada index 5:", list[5])
+print("Nilai baru ada pada index 9:", list[9])
+print(list)
+list.extend(['Rara','Nana'])
+print(list)
+print(list*4)
+print(len(list))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
