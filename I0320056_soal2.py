6514e7e4ceb509e3e02c2fe32f0f745be894d89c
MariaLaveniaVikaPamukasari <vikalavenia@gmail.com>
2021-03-19 22:32:04 +0700

    membuat soal 2

--- I0320056_soal2.py
+++ I0320056_soal2.py
@@ -1,1 +1,19 @@
-dict = {'Nama':'Vicka','Hobi':'Menari','Menyayi'}
+dict = {'Nama':'Vicka', 'Hobi':['menari','menyayi','berenang'], 'Sosial media':['vickasarry_','vikasarry02','Vicka Sarry'], 'Lagu':['melukis senja','bertahan','mengejar mimpi'], 'Makanan':['klepon','ayam geprek','gado-gado']}
+print("dict['Nama']:", dict['Nama'])
+print("dict['Hobi']:", dict['Hobi'])
+print("dict['Sosial media']:", dict['Sosial media'])
+print("dict['Lagu']:", dict['Lagu'])
+print("dict['Makanan']:", dict['Makanan'])
+
+
+#Mengubah salah satu hobi dan sosial media
+dict['Hobi'][1] = 'Membaca'
+dict['Sosial media'][2] = '@Vickasarry'
+print("dict['Hobi'[1]:", dict['Hobi'][1])
+print("dict:", dict['Sosial media'][2])
+#Menghapus dua makanan
+del dict['Makanan'][:2]
+print("dict['Makanan'][:2]:", dict['Makanan'][:2])
+#Menambahkan satu hobi
+dict['Hobi'] = 'Main gitar'
+print("dict['Hobi']:", dict['Hobi'])
