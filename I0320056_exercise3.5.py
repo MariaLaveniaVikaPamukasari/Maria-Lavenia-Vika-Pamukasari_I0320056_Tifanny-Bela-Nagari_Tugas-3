ba5bbdf37e0483596a728c7a1caae355571f9480
MariaLaveniaVikaPamukasari <vikalavenia@gmail.com>
2021-03-19 22:02:01 +0700

    merevisi pengulangan

--- I0320056_exercise3.5.py
+++
@@ -1,3 +0,0 @@
-dict = {'Name':'Vicka','Age':18,'Class':'Second'}
-print("dict['Name']:", dict['Name'])
-print("dict['Age']:", dict['Age'])
--- I0320056_soal1.py
+++ I0320056_soal1.py
@@ -1,21 +1,27 @@
+#Mengisi list dengan 10 nama teman
 list = ['Issa','Hafiz','Naufal','Harry','Ivan','Elsa','Rian','Ilham','Diva','Asyer']
 print(list)
+#Mengisi list indeks 4,6,7
 print("list[4]:", list[4])
 print("list[6:8]:", list[6:8])
+#Mengubah nama teman di list 3,5,9
 print("Nilai ada pada index 3:", list[3])
 print("Nilai ada pada index 5:", list[5])
 print("Nilai ada pada index 9:", list[9])
-list[3] = ('Ojat')
-list[5] = ('Alam')
-list[9] = ('Maurich')
+list[3] = 'Ojat'
+list[5] = 'Alam'
+list[9] = 'Maurich'
 print("Nilai baru ada pada index 3:", list[3])
 print("Nilai baru ada pada index 5:", list[5])
 print("Nilai baru ada pada index 9:", list[9])
 print(list)
+#Menambahkan 2 teman baru
 list.extend(['Rara','Nana'])
 print(list)
+#Menampilkan semua teman dengan perulangan
 for nama_teman in list:
     print(nama_teman)
+#Menampilkan panjang list
 print(len(list))
 
 
